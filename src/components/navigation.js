e13057bd1ae82900bc3894f27c7233b8eaa3c1c6
Rob Contreras <rob@Robs-iMac.local>
2018-09-25 20:31:49 -0600

    wip

---
+++ src/components/navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link text-primary" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}; export default Navigation;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import PopperJS from 'popper.js';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
